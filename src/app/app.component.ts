9a593e9f2a5bc154e0772b2f7412a3d354aa0b35
Pablo <pablomariano.moreno@nucleoo.com>
2023-03-10 12:24:04 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tfg-front';
+  toppings = this._formBuilder.group({
+    pepperoni: false,
+    extracheese: false,
+    mushroom: false,
+  });
+  //TODO: https://hackernoon.com/building-progressive-web-application-pwa-with-angular
+  constructor(private _formBuilder: FormBuilder) {
+  }
 }
