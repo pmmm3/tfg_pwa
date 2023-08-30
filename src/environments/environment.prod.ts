f0304c1fec7ef3117714a6f19867b38668bf41fc
pablo <pablo.triplem@gmail.com>
2023-08-30 18:46:04 +0200

    Change docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,9 @@ import { EnvironmentInterface } from '../interfaces/environment.interface';
 
 export const environment: EnvironmentInterface = {
   production: true,
-  frontendDomain: 'www.example.com',
-  apiDomain: 'api.example.com',
+  frontendDomain: 'bahia.ugr.es:42000',
+  apiDomain: 'bahia.ugr.es',
   apiProtocol: 'https',
   apiPath: '',
+  apiPort: 43000,
 };
